649e429b8acb5c55256cb4d824b231c5496009a5
anacardona0220 <anamariacb0220@gmail.com>
2023-05-30 13:48:45 -0500

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 base = str.split(' ') 
-print(base[5] + ' ' + base[6] + ' ' + base[12] + ' ' + base[0])
+print(base[5]+' '+ base[6]+' '+base[12]+' '+base[0])
