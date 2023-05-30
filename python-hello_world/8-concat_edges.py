197f380c8ec178155d3eabdb9816fcd0566e11aa
anacardona0220 <anamariacb0220@gmail.com>
2023-05-30 13:51:38 -0500

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-base = str.split(' ') 
-print(base[5]+' '+ base[6]+' '+base[12]+' '+base[0])
+base = str.split(' ')
+print(base[5]+' '+base[6]+' '+base[12]+' '+base[0])
