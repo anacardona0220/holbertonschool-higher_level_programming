22b9c5eddaa135509f764d5d72d24127dae87dd1
anacardona0220 <anamariacb0220@gmail.com>
2023-05-31 17:13:25 -0500

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,3 +4,4 @@ for i in range(100):
         print("{:02d}".format(i), end="\n")
         continue
     print("{:02d}{}{}".format(i, ',', ' '), end="")
+    # print("{:02d}".format(item), end=", " if item < 99 else "\n") otra forma de hacerlo
