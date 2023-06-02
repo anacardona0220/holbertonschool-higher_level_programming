06e6b2e8986e11613f8088f9c340daa2696c4331
anacardona0220 <anamariacb0220@gmail.com>
2023-06-02 15:36:35 -0500

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     sorted_names = sorted(name for name in names if not name.startswith('__'))
     for name in sorted_names:
-        print (name)
+        print(name)
