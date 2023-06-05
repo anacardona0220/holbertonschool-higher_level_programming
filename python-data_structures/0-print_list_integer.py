ef5720a03ef34bf95aa0030610c2c76ff9436abf
anacardona0220 <anamariacb0220@gmail.com>
2023-06-05 10:20:03 -0500

    Print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for item in range(1, len(my_list) + 1):
-        print("f{:d}".format(item))
+    for i in my_list:
+        print("f{:d}".format(i))
