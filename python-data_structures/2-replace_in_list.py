af64069fcff8e2982e4141aa546823177279d053
anacardona0220 <anamariacb0220@gmail.com>
2023-06-05 10:48:35 -0500

    Secure access to an element in a list

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
