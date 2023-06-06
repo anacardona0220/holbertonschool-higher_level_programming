aa52ba950701b9e6331c14e96aa01689000e5e26
anacardona0220 <anamariacb0220@gmail.com>
2023-06-06 13:57:31 -0500

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,10 +3,10 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return None
     else:
-        div_2 = my_list[:]
+        div_2 = []
         for i in my_list:
             if i % 2 == 0:
-                div_2[i] = True
+                div_2.append(True)
             else:
-                div_2[i] = False
+                div_2.append(False)
         return div_2
