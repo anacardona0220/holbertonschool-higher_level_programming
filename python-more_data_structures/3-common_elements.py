1271513877f383d74a48e58a7965c9acb2b6d076
anacardona0220 <anamariacb0220@gmail.com>
2023-06-08 15:07:45 -0500

    Only differents

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,3 +2,12 @@
 def common_elements(set_1, set_2):
 
     return set_1.intersection(set_2)
+
+# c_element = set()
+    # for i in set_1:
+    #     if i in set_2:
+    #         c_element.add(i)
+    # return c_element    
+
+    # c_element = set(filter(lambda x: x in set_1, set_2))
+    # return c_element  
