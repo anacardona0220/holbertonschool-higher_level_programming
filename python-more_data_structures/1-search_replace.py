ffa75f4dc426983ad30f63044d8a6d2b3fd2b7d8
anacardona0220 <anamariacb0220@gmail.com>
2023-06-08 15:00:12 -0500

    Present in both

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -7,3 +7,11 @@ def search_replace(my_list, search, replace):
         else:
             new_list.append(i)
     return new_list
+ # c_element = set()
+    # for i in set_1:
+    #     if i in set_2:
+    #         c_element.add(i)
+    # return c_element    
+
+    # c_element = set(filter(lambda x: x in set_1, set_2))
+    # return c_element  
--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     return (sum(set(my_list)))
+
+
+def uniq_add(my_list=[]):
+    for in my_list:
+        if 
+        
