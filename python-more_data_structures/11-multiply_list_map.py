13f80052f17a1b6fd870fcb89e4468e9247e80b2
anacardona0220 <anamariacb0220@gmail.com>
2023-06-09 11:58:26 -0500

    Multiply by using map

---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+   return list(map(lambda x: x * number, my_list))
+   
+   
+    # new_list = []
+    # for i in my_list:
+    #     new_list.append(i * number)
+    # return new_list
