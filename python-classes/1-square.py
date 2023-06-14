4fd3a7a47bd1fdced10a1c5d6545586bc0ca5765
anacardona0220 <anamariacb0220@gmail.com>
2023-06-14 10:50:01 -0500

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' create  empty class'''
+
+
+class Square:
+
+    '''definition class square'''
+
+    def __init__(self, size):
+        ''' Private instance attribute:size
+        Args:
+            param1 (size): Description of `param1`.'''
+        self.__size = size
