7a153b7802d3900d6a7e4890a023e237ca577fd1
anacardona0220 <anamariacb0220@gmail.com>
2023-06-20 16:07:54 -0500

    Eval is magic

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-"""
-This module define a Rectangle based in the
-previous module
-"""
+"""a class Rectangle that defines a rectangle"""
 
 
 class Rectangle:
     """
-    This class contain constructor and methods
+    Class properties of rectangle by: (based on 0-rectangle.py).
     """
 
     def __init__(self, width=0, height=0):
-        """
-        The method constructor:
-        Parameters:
-        width: the purpose is set the value
-        height: the purpose is set the value
-        """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """The getter parameters: itself"""
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        """The setter parameters: value"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
-        """The getter parameters: itself"""
         return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
-        """The setter parameters: value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.__width * self.__height
@@ -58,11 +47,9 @@ class Rectangle:
             return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-
         rectangle = ""
-
         if self.__width > 0 and self.__height > 0:
-            for i in range(self.__height):
+            for item in range(self.__height):
                 rectangle += '#' * self.__width + '\n'
         return rectangle.rstrip('\n')
 
