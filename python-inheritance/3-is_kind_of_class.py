f2b48aad08ccec6234b5cb9416b08e574eb4e306
anacardona0220 <anamariacb0220@gmail.com>
2023-06-21 14:37:50 -0500

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""object is an instance of a class that inherited"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method that return True if an object is an instance of a class"""
+    return isinstance(obj, a_class)
