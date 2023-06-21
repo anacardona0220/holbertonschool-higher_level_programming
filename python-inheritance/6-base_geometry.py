8c670598b70cf467e1455bd691dd7097666912d4
anacardona0220 <anamariacb0220@gmail.com>
2023-06-21 15:04:34 -0500

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
