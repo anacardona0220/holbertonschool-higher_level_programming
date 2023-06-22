76020a3c4767bb7697f7e8b287647cfbb0a30930
anacardona0220 <anamariacb0220@gmail.com>
2023-06-22 11:38:38 -0500

    square2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Square class that inherits from the Rectangle class.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square, which is a special case of a rectangle.
+
+    Attributes:
+        size (int): The size of the square (equal width and height).
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            ValueError: If size is not a positive integer.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+        def __str__(self, size):
+            return "[Square] {}/{}".format(self.__width, self.__height)
