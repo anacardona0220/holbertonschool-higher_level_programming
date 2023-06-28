5d3e51fdbeae6c785d453c12decf9886bc04ac06
anacardona0220 <anamariacb0220@gmail.com>
2023-06-28 13:21:24 -0500

    First Rectangle

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,69 @@
+import unittest
+from models.rectangle import Rectangle
+
+class RectangleTestCase(unittest.TestCase):
+    def test_valid_parameters(self):
+        # Requirement 12: Rectangle(1, 2)
+        rectangle1 = Rectangle(1, 2)
+        self.assertEqual(rectangle1.width, 1)
+        self.assertEqual(rectangle1.height, 2)
+        self.assertEqual(rectangle1.x, 0)
+        self.assertEqual(rectangle1.y, 0)
+
+        # Requirement 13: Rectangle(1, 2, 3)
+        rectangle2 = Rectangle(1, 2, 3)
+        self.assertEqual(rectangle2.width, 1)
+        self.assertEqual(rectangle2.height, 2)
+        self.assertEqual(rectangle2.x, 3)
+        self.assertEqual(rectangle2.y, 0)
+
+        # Requirement 14: Rectangle(1, 2, 3, 4)
+        rectangle3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rectangle3.width, 1)
+        self.assertEqual(rectangle3.height, 2)
+        self.assertEqual(rectangle3.x, 3)
+        self.assertEqual(rectangle3.y, 4)
+
+    def test_invalid_parameters(self):
+        # Requirement 15: Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            rectangle4 = Rectangle("1", 2)
+
+        # Requirement 16: Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            rectangle5 = Rectangle(1, "2")
+
+        # Requirement 17: Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            rectangle6 = Rectangle(1, 2, "3")
+
+        # Requirement 18: Rectangle(1, 2, 3, "4")
+        with self.assertRaises(TypeError):
+            rectangle7 = Rectangle(1, 2, 3, "4")
+
+        # Requirement 20: Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            rectangle8 = Rectangle(-1, 2)
+
+        # Requirement 21: Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            rectangle9 = Rectangle(1, -2)
+
+        # Requirement 22: Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            rectangle10 = Rectangle(0, 2)
+
+        # Requirement 23: Rectangle(1, 0)
+        with self.assertRaises(ValueError):
+            rectangle11 = Rectangle(1, 0)
+
+        # Requirement 24: Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            rectangle12 = Rectangle(1, 2, -3)
+
+        # Requirement 25: Rectangle(1, 2, 3, -4)
+        with self.assertRaises(ValueError):
+            rectangle13 = Rectangle(1, 2, 3, -4)
+
+if __name__ == '__main__':
+    unittest.main()
