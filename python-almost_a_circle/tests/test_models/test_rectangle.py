2db835e426e735c8e704ac01c5a999e9297296cb
anacardona0220 <anamariacb0220@gmail.com>
2023-06-30 13:46:10 -0500

    testsquare

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,7 @@ import unittest
 from models.rectangle import Rectangle
 from unittest.mock import patch
 import io
+from io import StringIO
 
 
 class RectangleTestCase(unittest.TestCase):
@@ -50,53 +51,83 @@ class RectangleTestCase(unittest.TestCase):
             Rectangle(0, 2)
         with self.assertRaises(ValueError):
             Rectangle(1, 0)
-        # with self.assertRaises(TypeError):
-        #     rectangle8 = Rectangle(1,2,3,4,5)
+            
+            def test_rectangle_area(self):
+        rect = Rectangle(4, 5)
+        self.assertEqual(rect.area(), 20)
+        rect.width = 10
+        rect.height = 3
+        self.assertEqual(rect.area(), 30)
 
-    # def test_area_calculate(self):
-    #     rectangle9 = Rectangle(2, 6)
-    #     area = rectangle9.area()
-    #     self.assertEqual(area, 12)
+    def test_rectangle_str(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        expected_output = "[Rectangle] (1) 2/3 - 4/5"
+        self.assertEqual(str(rect), expected_output)
 
-    # def test_display_without_xy(self):
-    #     rect = Rectangle(4, 5, 0, 0, 10)
-    #     expected_output = "####\n####\n####\n####\n####\n"
-    #     self.assertEqual(rect.display(), expected_output)
+    def test_display(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        expected_output = "\n\n\n  ####\n  ####\n  ####\n  ####\n  ####\n"
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
+            rect.display()
+            self.assertEqual(fake_out.getvalue(), expected_output)
 
-    # def test_display_without_y(self):
-    #     rect = Rectangle(4, 5, 2, 0, 10)
-    #     expected_output = "  ####\n  ####\n  ####\n  ####\n  ####\n"
-    #     self.assertEqual(rect.display(), expected_output)
+    def test_display_one_arg(self):
+        r = Rectangle(5, 1, 2, 4, 7)
+        with self.assertRaises(TypeError):
+            r.display(1)
 
-    
+    def test_rectangle_display_without_x_y(self):
+        rectangle = Rectangle(5, 3)
+        expected_output = "#####\n#####\n#####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    # def setUp(self):
-    #     # Runs before every test
-    #     self.rectangle10 = Rectangle(4, 5)  # Rectangle without x and y
-    #     self.rectangle11 = Rectangle(4, 5, x=2)  # Rectangle without y
-    #     self.rectangle12 = Rectangle(4, 5, x=2, y=3)  # Rectangle with x and y
+    def test_rectangle_display_without_y(self):
+        rectangle = Rectangle(5, 3, 2)
+        expected_output = "  #####\n  #####\n  #####\n"
+        with unittest.mock.patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            rectangle.display()
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    # def test_display_without_x_and_y(self):
-    #     expected_output = "####\n####\n####\n####\n####\n"
-    #     with patch("sys.stdout", new=io.StringIO()) as fake_output:
-    #         self.rectangle10.display()
-    #         self.assertEqual(fake_output.getvalue(), expected_output)
+    def test_rectangle_update_with_args(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(2, 6, 7, 1, 4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 4)
 
-    # def test_display_without_y(self):
-    #     expected_output = "\n\n  ####\n  ####\n  ####\n  ####\n  ####\n"
-    #     with patch("sys.stdout", new=io.StringIO()) as fake_output:
-    #         self.rectangle11.display()
-    #         self.assertEqual(fake_output.getvalue(), expected_output)
+    def test_rectangle_update_with_kwargs(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(id=2, width=6, height=7, x=1, y=4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 4)
 
-    # def test_display(self):
-    #     expected_output = "    ####\n    ####\n    ####\n    ####\n    ####\n"
-    #     with patch("sys.stdout", new=io.StringIO()) as fake_output:
-    #         self.rectangle12.display()
-    #         self.assertEqual(fake_output.getvalue(), expected_output)
+    def test_rectangle_update_with_args_and_kwargs(self):
+        rect = Rectangle(4, 5, 2, 3, 1)
+        rect.update(2, width=6, height=7, y=4)
+        self.assertEqual(rect.id, 2)
+        self.assertEqual(rect.width, 6)
+        self.assertEqual(rect.height, 7)
+        self.assertEqual(rect.x, 2)
+        self.assertEqual(rect.y, 4)
 
-    def test_str(self):
-        rectan = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(str(rectan), "[Rectangle] (5) 3/4 - 1/2")
+    def test_to_dictionary(self):
+        rect = Rectangle(5, 10, 2, 3, 1)
+        rect_dict = rect.to_dictionary()
+        expected_dict = {
+            "x": 2,
+            "y": 3,
+            "id": 1,
+            "height": 10,
+            "width": 5
+        }
+        self.assertDictEqual(rect_dict, expected_dict)
 
 if __name__ == '__main__':
     unittest.main()
